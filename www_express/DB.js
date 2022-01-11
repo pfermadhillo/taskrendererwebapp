8266479e94e4435273ca066a4648cfa54223c391
pfermadhillo <73960476+pfermadhillo@users.noreply.github.com>
2022-01-11 16:53:54 -0600

    Delete DB.js

--- www_express/DB.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	// DB: 'mongodb://valnya:dyr123@ds241278.mlab.com:41278/valnya'
-	DB: 'mongodb+srv://atlas:atlas432@cluster0.ovndq.mongodb.net/TaskRendererDB?retryWrites=true&w=majority'
-}
